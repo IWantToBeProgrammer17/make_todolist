e1f7d7c9c8aa9c5282288efd066eb300b48de8cb
IwantToBeProgrammer17 <azky.haekal35@smp.belajar.id>
2025-08-31 11:50:42 +0700

    refactor: update API endpoints to include '/api/' prefix and add HTML views for categories and todos

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const path = require('path');
 const { connectToDB } = require('./models');
 const userRouter = require('./routes/users');
 const todosRouter = require('./routes/todos');
 const categoryRouter = require('./routes/categories');
 const app = express();
 app.use(express.json());
+app.use(express.static('public'));
 
 app.get('/', function(request, response){
     response.send('Hello World!');
@@ -14,11 +16,17 @@ app.get('/about', function(request, response){
     response.send('<h2 style="color: skyblue;">About Page</h2>');
 })
 
-app.use('/categories', categoryRouter);
-app.use('/users', userRouter);
-app.use('/todos', todosRouter);
+app.use('/api/categories', categoryRouter);
+app.use('/api/users', userRouter);
+app.use('/api/todos', todosRouter);
 
+app.get('/todos', async function(request, response){
+    return response.sendFile(path.join(__dirname + '/views/todos/index.html'));
+});
 
+app.get('/categories', async function(request, response){
+    return response.sendFile(path.join(__dirname + '/views/categories/index.html'));
+});
 
 
 
